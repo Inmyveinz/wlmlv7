fb6c39c6a8169b2912e8ec24923de8714bcb08b0
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2022-10-22 11:40:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -818,7 +818,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('ᴍᴏʀᴇ', callback_data='eth')
         ], [
             InlineKeyboardButton('ʜᴏᴡ ᴛᴏ ᴜsᴇ', url='https://t.me/HowToUseMe'),
-            InlineKeyboardButton('ɴᴏᴛ ᴜᴘᴅᴀᴛᴇs', url='https://t.me/FinderUpdates')
+            InlineKeyboardButton('ʙᴏᴛ ᴜᴘᴅᴀᴛᴇs', url='https://t.me/FinderUpdates')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
@@ -1588,7 +1588,7 @@ async def advantage_spell_chok(msg):
         await asyncio.sleep(8)
         await k.delete()
         return
-    SPELL_CHECK[msg.message_id] = movielist
+    SPELL_CHECK[msg.id] = movielist
     btn = [[
         InlineKeyboardButton(
             text=movie.strip(),
@@ -1605,7 +1605,7 @@ async def advantage_spell_chok(msg):
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
     name = text or message.text
-    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
     keywords = await get_filters(group_id)
     for keyword in reversed(sorted(keywords, key=len)):
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
@@ -1627,14 +1627,14 @@ async def manual_filters(client, message, text=False):
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button),
-                                reply_to_message_id=reply_id
+                                reply_to_msg_id=reply_id
                             )
                     elif btn == "[]":
                         await client.send_cached_media(
                             group_id,
                             fileid,
                             caption=reply_text or "",
-                            reply_to_message_id=reply_id
+                            reply_to_msg_id=reply_id
                         )
                     else:
                         button = eval(btn)
@@ -1642,7 +1642,7 @@ async def manual_filters(client, message, text=False):
                             fileid,
                             caption=reply_text or "",
                             reply_markup=InlineKeyboardMarkup(button),
-                            reply_to_message_id=reply_id
+                            reply_to_msg_id=reply_id
                         )
                 except Exception as e:
                     logger.exception(e)
